99d6bacccf7a58c2fd62797dec624eb8af603ada
chunxubioinfor <chunxuhan007@gmail.com>
2025-08-20 10:01:11 +0200

    prepare data

--- GTEx_data_prepare.R
+++ GTEx_data_prepare.R
@@ -2,7 +2,7 @@
 ## wget https://zenodo.org/records/6826603/files/Performance_GTEx.zip
 
 # GTEx_data_prepare.R
-# - Homogeneous GTEx subset
+# - Homogeneous GTEx subset (This step is too memory consuming, so run on server)
 # - Generate 5v5 simulated DTU dataset (1 repeat)
 # - NO pre-ISAR filtering (we'll filter inside ISAR later)
 # - Save .Rdata with counts + design + truth
@@ -130,8 +130,10 @@ gtexSample <- gtexSample[which(gtexSample$SMCENTER == 'B1'),]
 gtexCm <- gtexCm[,which(
   colnames(gtexCm) %in% gtexSample$SAMPID
 )]
+# save(gtexSample,gtexCm,txInfo,file = 'GTEx_subset.Rdata')
 
 # ---------------- Simulation settings ----------------
+load("~/Desktop/ISAR/ISAR_benchmark/data/GTEx_subset.Rdata")
 samplesPrCondition <- 5L  # 5 vs 5
 nrRepsMade         <- 1L  # one repeat
 fracGenesAffected  <- 0.15
@@ -172,4 +174,33 @@ cat("\nSummary (first/only replicate):\n",
     length(unique(obj$metaInfo$GENEID[obj$metaInfo$txSwapped])), " genes\n",
     sep = "")
 
+# ---------------- Sanity Checks ----------------
+load(file.path(data_dir, "GTEx_benchmark_5v5_noFilter.Rdata"))
+obj <- gtexBenchmarkData_NoFilter[[1]]
+# 1) Dimensions & alignment
+stopifnot(ncol(obj$data) == 10)                  # 5 + 5
+stopifnot(all(colnames(obj$data) == obj$design$sample_id))
+stopifnot(all(obj$design$condition %in% c("a","b")))
+table(obj$design$condition)                      # should be 5 a / 5 b
+# 2) No duplicated isoform IDs, all from metaInfo
+stopifnot(!any(duplicated(rownames(obj$data))))
+stopifnot(all(rownames(obj$data) %in% obj$metaInfo$TXNAME))
+# 3) Multi-isoform property is respected
+iso_per_gene <- table(obj$metaInfo$GENEID[match(rownames(obj$data), obj$metaInfo$TXNAME)])
+stopifnot(all(iso_per_gene >= 2))
+# 4) Truth labels make sense
+sum_tx_swapped <- sum(obj$metaInfo$txSwapped)
+sum_gene_swapped <- length(unique(obj$metaInfo$GENEID[obj$metaInfo$txSwapped]))
+cat("Swapped isoforms:", sum_tx_swapped, " | Swapped genes:", sum_gene_swapped, "\n")
+stopifnot(sum_gene_swapped >= 1)
+# 5) Rough affected fraction ≈ fracGenesAffected (by genes with >=2 isoforms)
+multi_genes <- unique(obj$metaInfo$GENEID)
+frac_obs <- sum_gene_swapped / length(multi_genes)
+cat(sprintf("Observed affected fraction: %.3f (target %.3f)\n", frac_obs, 0.15))
+# 6) Spot-check a modified gene to see the permutation effect in B
+mod_gene <- unique(obj$metaInfo$GENEID[obj$metaInfo$txSwapped])[1]
+txs <- obj$metaInfo$TXNAME[obj$metaInfo$GENEID == mod_gene]
+A <- obj$data[txs, obj$design$sample_id[obj$design$condition=="a"], drop=FALSE]
+B <- obj$data[txs, obj$design$sample_id[obj$design$condition=="b"], drop=FALSE]
 
+## Check done!
